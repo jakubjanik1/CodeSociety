1467bc4973ce58ad2e246f4c3d84bf4f71251fe0
Jakub Janik <jakubjanik@vp.pl>
2019-03-03 12:51:30 +0100

    Extend Query Builder by adding some basic methods
    Add new methods to QueryBuilder class such as take,
    skip, select, where and table. I can chain the calls
    of this methods.

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -7,18 +7,64 @@ use PDO;
 class QueryBuilder
 {
     private $pdo;
+    private $table;
 
     public function __construct($pdo) 
     {
         $this->pdo = $pdo;
     }
 
-    public function selectAll($table)
+    public function table($name)
     {
-        $statement = $this->pdo->prepare("select * from {$table}");
+        $statement = $this->pdo->prepare("select * from {$name}");
 
         $statement->execute();
         
-        return $statement->fetchAll(PDO::FETCH_CLASS);
+        $this->table = $statement->fetchAll(PDO::FETCH_CLASS);
+
+        return $this;
+    }
+
+    public function select(...$fields)
+    {
+        $this->table = array_map(function($item) use ($fields) {
+            $obj = new \stdClass();
+            foreach ($fields as $field) {
+                if (isset($item->$field)) {
+                    $obj->$field = $item->$field;
+                }
+            }
+            return $obj;
+        }, $this->table);
+
+        return $this;
+    }
+
+    public function where($field, $value)
+    {
+        $this->table = array_filter($this->table, function($item) use ($field, $value) {
+            return $item->$field == $value;
+        });
+
+        return $this;
+    }
+
+    public function skip($offset)
+    {
+        $this->table = array_slice($this->table, $offset);
+
+        return $this;
+    }
+
+    public function take($count)
+    {
+        $this->table = array_slice($this->table, 0, $count);
+
+        return $this;
+    }
+
+    public function get()
+    {
+        return $this->table;
     }
 }
