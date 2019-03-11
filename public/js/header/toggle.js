391ae6ccada24fcaecdc65e8fba7373e6751f13d
Jakub Janik <jakubjanik@vp.pl>
2019-03-11 22:13:42 +0100

    Improve mobile version of header
    Change and refactor toggle.js to load hidden
    header at start. It prevent from flicking a
    menu on the first load.

--- public/js/header/toggle.js
+++ public/js/header/toggle.js
@@ -1,49 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let header = document.querySelector('.header');
     let menu = document.querySelector('.header__menu');
     let searchForm = document.querySelector('.header__search-form');
     let items = document.querySelector('.header__items');
-    let logo = document.querySelector('.header__logo');
     let toggle = document.querySelector('.header__toggle');
 
-    window.addEventListener('resize', hideHeader);
-    window.addEventListener('load', hideHeader);
-
-    toggle.addEventListener('click', function () {
-        if (menu.classList.contains('hidden')) {
-            expandHeader();
-        } else {
-            collapseHeader();
+    window.addEventListener('resize', function (event) {
+        if (event.target.innerWidth > 1130) {
+            menu.classList.remove('visible');
+            searchForm.classList.remove('visible');
+            items.classList.remove('visible');
         }
-
-        menu.classList.toggle('hidden');
-        searchForm.classList.toggle('hidden');
-        items.classList.toggle('hidden');
     });
 
-    let once = false;
-    function hideHeader(event) {
-        let width = event.currentTarget.innerWidth;
-
-        if (!menu.classList.contains('hidden') && width < 1130 && !once) {
-            toggle.click();
-            once = true;
-            expandHeader();
-        } else if (width > 1130) {
-            once = false;
-            collapseHeader();
-        }
-    }
-
-    function expandHeader() {
-        header.style.height = 'auto';
-        header.style['padding-bottom'] = '18px';
-        logo.style['margin-top'] = '19px';
-    }
-
-    function collapseHeader() {
-        header.style.height = '80px';
-        header.style.padding = 0;
-        logo.style['margin-top'] = 0;
-    }
+    toggle.addEventListener('click', function () {
+        menu.classList.toggle('visible');
+        searchForm.classList.toggle('visible');
+        items.classList.toggle('visible');
+    });
 });
