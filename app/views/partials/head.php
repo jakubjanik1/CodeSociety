9ac12d5d7047fdd1221ea1a5bf8d3a959419db92
Jakub Janik <jakubjanik@vp.pl>
2019-03-20 22:00:59 +0100

    Automate linking styles and scripts

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -5,28 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CodeSociety</title>
     
-    <link rel="stylesheet" href="/public/css/header/header.css">
-    <link rel="stylesheet" href="/public/css/header/logo.css">
-    <link rel="stylesheet" href="/public/css/header/menu.css">
-    <link rel="stylesheet" href="/public/css/header/search-form.css">
-    <link rel="stylesheet" href="/public/css/header/items.css">
-    <link rel="stylesheet" href="/public/css/header/toggle.css">
-    <link rel="stylesheet" href="/public/css/variables.css">
-    <link rel="stylesheet" href="/public/css/content.css">
-    <link rel="stylesheet" href="/public/css/articles/title.css">
-    <link rel="stylesheet" href="/public/css/articles/articles.css">
-    <link rel="stylesheet" href="/public/css/articles/categories.css">
-    <link rel="stylesheet" href="/public/css/articles/list.css">
-    <link rel="stylesheet" href="/public/css/articles/article.css">
+    <?php includeStyles() ?>
 
-    <link href='https://fonts.googleapis.com/css?family=Dosis:500,600,700' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Dosis:500,600,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-    <script src="/public/js/header/search-form.js"></script>
-    <script src="/public/js/header/toggle.js"></script>
-    <script src="/public/js/header/menu.js"></script>
-    <script src="/public/js/articles/categories.js"></script>
-    <script src="/public/js/articles/article.js"></script>
+    <?php includeScripts() ?>
+
 </head>
 <body>
     <?php require 'header.php' ?>
--- core/helpers.php
+++ core/helpers.php
@@ -6,3 +6,19 @@ function view($name, $data = [])
 
     require "app/views/$name.view.php";
 }
+
+function includeStyles()
+{
+    foreach (glob('public/css{/*,,}/*.css', GLOB_BRACE) as $file)
+    {
+        echo "<link rel='stylesheet' href='/{$file}'>\n";
+    }
+}
+
+function includeScripts()
+{
+    foreach (glob('public/js{/*,,}/*.js', GLOB_BRACE) as $file)
+    {
+        echo "<script src='/{$file}'></script>\n";
+    }
+}
